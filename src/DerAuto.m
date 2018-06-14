9bab37c822e1b942d1443859355bc1ccbb5219d1
Peter Brooksbank <pbrooksb@pbrooksb.bucknell.edu>
2018-06-14 10:43:51 -0400

    PAB finished fledgling version of Lie module similarity for semisimple Lie algebras

--- src/DerAuto.m
+++ src/DerAuto.m
@@ -420,56 +420,301 @@ return forall { i : i in [1..Ngens (J)] | forall { t : t in [1..Ngens (U)] |
 end function;
 
 intrinsic SimilaritiesOfSemisimpleLieModule (L::AlgMatLie, d::RngIntElt :
-                E := [ ], F := [ ]) -> GrpMat
+                E := [ ], F := [ ], H := [ ]) -> GrpMat
 { Construct the group of similarites of the given (completely reducible) representation.}
        
      flag, LL := HasLeviSubalgebra (L);
-     require (flag and (L eq LL)): "L must be a semisimple algebra";
+     require (flag and (L eq LL)) : "L must be a semisimple algebra";
+     
+     if #E eq 0 then
+         E, F, H := ChevalleyBasis (L);
+     end if;
                                         
      k := BaseRing (L);
      n := Degree (L);
+     G := GL (n, k);
+     
      X := VectorSpace (k, n);
      V := sub < X | [ X.i : i in [1..d] ] >;
      W := sub < X | [ X.i : i in [d+1..n] ] >;
         
      M := RModule (L);
-     inds := IndecomposableSummands (ML);
-     inds := [ sub < X | [ Vector (ML!(S.i)) : i in [1..Dimension (S)] ] > : S in inds ];
+     
+     indM := IndecomposableSummands (M);
+"there are", #indM, "indecomposable summands   ", [ Dimension (U) : U in indM ];
+     require &+ [ Dimension (S) : S in indM ] eq n : 
+        "indecomposable summands do not span the space";
+     indX := [ sub < X | [ Vector (M!(S.i)) : i in [1..Dimension (S)] ] > : S in indM ];
+     
+     // first insert indecomposable summands upon which L acts trivially
+     V0subs := < U : U in indX | __annihilates (L, U) and (U subset V) >;
+"V0dims:", [ Dimension (U) : U in V0subs ];
+     W0subs := < U : U in indX | __annihilates (L, U) and (U subset W) >;
+"W0dims", [ Dimension (U) : U in W0subs ];
+     mV0 := 0;    mW0 := 0;
+     if (#V0subs gt 0) then
+        "L acts trivially on some summand of V";
+        mV0 +:= &+[ Dimension (U) : U in V0subs ];
+     end if;
+     if (#W0subs gt 0) then
+        "L acts trivially on some summand of W";
+        mW0 +:= &+[ Dimension (U) : U in W0subs ];
+     end if;
+"mV0 =", mV0;
+"mW0 =", mW0;
+     
+     indX := [ U : U in indX | not __annihilates (L, U) ];
+"there are", #indX, "indecomposable summands that are not annihilated by L   ",
+[ Dimension (U) : U in indX ];
+     
+// temporary sanity check: all summands lie in V or in W.
+assert (#indX + #V0subs + #W0subs) eq #indM;
+     
+     // for the rest of the indecomposable summands we require the
+     // support to be one single minimal ideal
      ideals := IndecomposableSummands (L);
-     // make sure each indecomposable summand corresponds to exactly one ideal.
-     flag := forall { U : U in inds | 
-                          #{ J : J in ideals | not __annihilates (J, U) } le 1 };
-     if not flag then
-     error "the code does not yet handle irreducibles whose ideal is not simple";
+     require forall { U : U in indX | 
+                #{ J : J in ideals | not __annihilates (J, U) } le 1 } :
+        "each summand of X must be an irreducible module for a minimal ideal of L";
+        
+     // collect together summands of V and W according to ideal
+     Vsubs := < >;    Vdims := < >;
+     Wsubs := < >;    Wdims := < >;
+     for i in [1..#ideals] do
+         J := ideals[i];
+         mJV := [ ];   mJW := [ ];
+         for U in indX do
+             if not __annihilates (J, U) then
+                 if U subset V then
+                     Append (~Vsubs, U);
+                     Append (~mJV, Dimension (U));
+                 else
+                     assert U subset W;   // again, all summands lie in V or W
+                     Append (~Wsubs, U);
+                     Append (~mJW, Dimension (U));
+                 end if;
+             end if;
+         end for;
+         Append (~Vdims, mJV);
+         Append (~Wdims, mJW);
+     end for;
+"Vdims =", Vdims;
+"Wdims =", Wdims;
+          
+     C := Matrix (
+                  (&cat [ Basis (U) : U in Vsubs ]) cat
+                  (&cat [ Basis (U) : U in V0subs ]) cat
+                  (&cat [ Basis (U) : U in Wsubs ]) cat
+                  (&cat [ Basis (U) : U in W0subs ])  
+                );
+     
+     LL := sub < Generic (L) | [ C * Matrix (L.i) * C^-1 : i in [1..Ngens (L)] ] >;
+     // start with generators for the centralizer of LL
+     MM := RModule (LL);
+     AA := EndomorphismAlgebra (MM);
+     isit, Cent := UnitGroup (AA); assert isit;
+"centralizer of L has order", #Cent;
+     gens := [ Cent.i : i in [1..Ngens (Cent)] ];
+     
+     II := [ sub < Generic (J) | [ C * Matrix (J.i) *C^-1 : i in [1..Ngens (J)] ] > :
+                     J in ideals ];
+     EE := [ C * Matrix (E[i]) * C^-1 : i in [1..#E] ];
+     FF := [ C * Matrix (F[i]) * C^-1 : i in [1..#F] ];
+     // exponentiate what we can
+     gens cat:= [ G!__exp(e) : e in EE ] cat [ G!__exp(f) : f in FF ];
+"after adding connected component, the normalizer has order", #sub<G|gens>;
+                     
+     // try to lift diagonal and graph automorphisms for each minimal ideal  
+     posV := 1;
+     posW := d + 1;
+     for i in [1..#II] do
+"i =", i;
+          Ji := II[i];
+          ti := SemisimpleType (Ji);
+assert #ti eq 2;    // assuming the rank is at most 9!!
+"ti =", ti;
+
+          EEi := [ e : e in EE | Generic (Ji)!e in Ji ];
+          FFi := [ f : f in FF | Generic (Ji)!f in Ji ];
+          
+          delta := Identity (MatrixAlgebra (k, n));  // diagonal auto
+              xi := PrimitiveElement (k);
+              ri := StringToInteger (ti[2]);
+              Si := [ xi ] cat [ k!1 : i in [1..ri] ];
+          gamma := Identity (MatrixAlgebra (k, n));  // graph auto
+              GAi := [ ];
+              if (ti[1] eq "A") and (ri ge 2) then
+                  Append (~GAi, Sym (ri)![ri + 1 - i : i in [1..ri] ]);
+              elif (ti[1] eq "D") then
+                  Append (~GAi, Sym (ri)!(ri-1,ri));
+                  if (ri eq 4) then
+                      Append (~GAi, Sym (ri)!(1,3,4));
+                  end if;
+              end if;
+"GAi =", GAi;
+          
+          // first handle the summands in V
+          for s in [1..#Vdims[i]] do
+              EEis := [ ExtractBlock (EEi[j], posV, posV, Vdims[i][s], Vdims[i][s]) :
+                         j in [1..#EEi] ];
+              FFis := [ ExtractBlock (FFi[j], posV, posV, Vdims[i][s], Vdims[i][s]) :
+                         j in [1..#FFi] ];
+              Jis := sub < MatrixLieAlgebra (k, Vdims[i][s]) |
+                      [ ExtractBlock (Ji.j, posV, posV, Vdims[i][s], Vdims[i][s]) :
+                         j in [1..Ngens (Ji)] ] >;
+assert IsIrreducible (RModule (Jis));
+              Cis, crys := CrystalBasis (Jis, 
+                          [EEis[x] : x in [1..ri]], [FFis[x] : x in [1..ri]]
+                                        );
+              
+              // lift diagonal auto
+              D0 := [ k!1 ];
+              for a in [2..#crys] do
+                  word := crys[a][2];  // the word labelling the i-th node 
+                  Append (~D0, &*[ Si[word[j]] / Si[1+word[j]] : j in [1..#word] ]);
+              end for;
+              D0 := DiagonalMatrix (D0);
+              Dis := Cis^-1 * D0 * Cis;
+assert NormalizesMatrixAlgebra (Jis, [Dis]);  // sanity check
+              InsertBlock (~delta, Dis, posV, posV);
+              
+              // lift graph auto
+              Bis := [ Vector (Cis[a]) : a in [1..Nrows (Cis)] ];
+              Vis := VectorSpaceWithBasis (Bis);
+              for s in GAi do
+                  g0 := [ ];
+                  for a in [1..#crys] do
+                      word := crys[a][2];
+                      gword := [ word[j]^s : j in [1..#word] ];           
+                      vec := Vis.1;
+                      for j in [1..#gword] do  
+                          vec := vec * FFis[gword[j]];
+                      end for;
+                      Append (~g0, Coordinates (Vis, vec));
+                  end for;
+                  g0 := Matrix (g0);
+                  if Rank (g0) eq Rank (Cis) then
+                      g := Cis^-1 * GL (Nrows (g0), k)!g0 * Cis;
+                      if NormalizesMatrixAlgebra (Jis, [g]) then
+                          InsertBlock (~gamma, Dis, posV, posV);
+                      else
+"   (graph auto did not lift to summand)"; 
+                      end if;
+                  else
+"   (graph auto did not lift to summand)"; 
+                  end if;
+              end for;
+              
+              posV +:= Vdims[i][s];
+              
+          end for;
+          
+          // next handle the summands in W
+          for s in [1..#Wdims[i]] do
+              EEis := [ ExtractBlock (EEi[j], posW, posW, Wdims[i][s], Wdims[i][s]) :
+                         j in [1..#EEi] ];
+              FFis := [ ExtractBlock (FFi[j], posW, posW, Wdims[i][s], Wdims[i][s]) :
+                         j in [1..#FFi] ];
+              Jis := sub < MatrixLieAlgebra (k, Wdims[i][s]) |
+                      [ ExtractBlock (Ji.j, posW, posW, Wdims[i][s], Wdims[i][s]) :
+                         j in [1..Ngens (Ji)] ] >;
+assert IsIrreducible (RModule (Jis));
+              Cis, crys := CrystalBasis (Jis, 
+                          [EEis[x] : x in [1..ri]], [FFis[x] : x in [1..ri]]
+                                        );
+              
+              // lift diagonal auto
+              D0 := [ k!1 ];
+              for a in [2..#crys] do
+                  word := crys[a][2];  // the word labelling the i-th node 
+                  Append (~D0, &*[ Si[word[j]] / Si[1+word[j]] : j in [1..#word] ]);
+              end for;
+              D0 := DiagonalMatrix (D0);
+              Dis := Cis^-1 * D0 * Cis;
+assert NormalizesMatrixAlgebra (Jis, [Dis]);  // sanity check
+              InsertBlock (~delta, Dis, posW, posW);
+              
+              // lift graph auto
+              Bis := [ Vector (Cis[a]) : a in [1..Nrows (Cis)] ];
+              Vis := VectorSpaceWithBasis (Bis);
+              for s in GAi do
+                  g0 := [ ];
+                  for a in [1..#crys] do
+                      word := crys[a][2];
+                      gword := [ word[j]^s : j in [1..#word] ];           
+                      vec := Vis.1;
+                      for j in [1..#gword] do  
+                          vec := vec * FFis[gword[j]];
+                      end for;
+                      Append (~g0, Coordinates (Vis, vec));
+                  end for;
+                  g0 := Matrix (g0);
+                  if Rank (g0) eq Rank (Cis) then
+                      g := Cis^-1 * GL (Nrows (g0), k)!g0 * Cis;
+                      if NormalizesMatrixAlgebra (Jis, [g]) then
+                          InsertBlock (~gamma, Dis, posV, posV);
+                      else
+"   (graph auto did not lift to summand)"; 
+                      end if;
+                  else
+"   (graph auto did not lift to summand)"; 
+                  end if;
+              end for;         
+              
+              posW +:= Wdims[i][s];
+          end for;
+          
+          if not delta eq Identity (G) then
+              Append (~gens, delta);
+"added delta =", delta;
+"normalizer now has order", #sub<G|gens>;
+          end if;
+          if not gamma eq Identity (G) then
+              Append (~gens, gamma);
+"added gamma =", gamma;
+"normalizer now has order", #sub<G|gens>;
+          end if;
+"---------";
+     
+     end for;
+  
+     // throw in GL generators for the summands of V and W annihilated by L
+// ACTUALLY THESE ALREADY OCCUR IN THE CENTRALIZER OF L, RIGHT?
+     if mV0 gt 0 then
+"adding generators for GL(nullspace) on V ...";
+"posV =", posV;
+         mV := &+ [ &+ Vdims[i] : i in [1..#Vdims] | #Vdims[i] gt 0 ];
+"mV =", mV;
+         GLmV := GL (mV0, k);
+         gens cat:= [ InsertBlock (Identity (MatrixAlgebra (k, n)), GLmV.i, posV, posV) :
+                        i in [1..Ngens (GLmV)] ];
+[ InsertBlock (Identity (MatrixAlgebra (k, n)), GLmV.i, posV, posV) :
+                        i in [1..Ngens (GLmV)] ];
      end if;
      
-     // organize the summands into those within V and those within W
-     Vsubs := < >;
-     Wsubs := < >;
-     for U in inds do
-         if U subset V then
-             Append (~Vsubs, U);
-         else
-             assert U subset W;
-             Append (~Wsubs, U);
-         end if;
-     end for;
+     if mW0 gt 0 then
+"adding generators for GL(nullspace) on W ...";
+"posW =", posW;
+         mW := &+ [ &+ Wdims[i] : i in [1..#Wdims] | #Wdims[i] gt 0 ];  
+"mW =", mW;
+         GLmW := GL (mW0, k);
+         gens cat:= [ InsertBlock (Identity (MatrixAlgebra (k, n)), GLmW.i, posW, posW) :
+                        i in [1..Ngens (GLmW)] ];
+     end if;
+"normalizer now has order", #sub<G|gens>;
+      
+// NEED Sn ACTIONS ON ISOTYPIC COMPONENTS     
+     
+     NN := sub < G | gens >; 
 
-     // within this basic division collect together summands according to ideal
-     nVsubs := [ ];
-     nWsubs := [ ];
-     for J in ideals do
-          nVsubs cat:= [ U : U in Vsubs | not __annihilates (J, U) ];
-          nWsubs cat:= [ U : U in Wsubs | not __annihilates (J, U) ];
-     end for; 
-//     assert #nVsubs eq #Vsubs;
-//     assert #nWsubs eq #Wsubs; 
-     C := Matrix ((&cat [ Basis (U) : U in Vsubs ]) cat 
-                   &cat [ Basis (U) : U in Wsubs ]);
+assert NormalizesMatrixAlgebra (LL, [NN.i : i in [1..Ngens (NN)]]); // final sanity check 
+
+     // conjugate back
+     N := sub < G | [ C^-1 * gens[i] * C : i in [1..#gens] ] >;    
      
-     LC := sub < Generic (L) | [ C * Matrix (L.i) * C^-1 : i in [1..Ngens (L)] ] >;   
+     assert NormalizesMatrixAlgebra (L, [N.i : i in [1..Ngens (N)]]); // final sanity check
      
-return LC;     
+return NN, N;     
 end intrinsic;
 
 
