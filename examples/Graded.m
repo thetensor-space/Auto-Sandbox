6e2a374b3b901cabc0118cc84b1226d3963554de
Peter A Brooksbank <pbrooksb@bucknell.edu>
2022-07-05 09:20:41 -0400

    minor edits

--- examples/Graded.m
+++ examples/Graded.m
@@ -1,14 +1,4 @@
 
-RandomGraded := function (K, dims)
-	ddims := func<i | (i le #dims) select dims[i] else 0 >;
-	rand := func< i,j | 
-			(i eq j ) select 
-				Random(AlternatingSpace(KTensorSpace(K,[dims[i],dims[i],ddims(i+i)]))) 
-			else
-				Random(KTensorSpace(K,[dims[i],dims[j],ddims(i+j)])) >;
-	return [* [* rand(i,j) : j in [i..#dims] *] : i in [1..#dims] *];
-end function;
-
 UpperTriangularLie := function (d, p)
   G := ClassicalSylow (GL (d,p), p);
   U := PCPresentation (UnipotentMatrixGroup (G));
@@ -24,7 +14,7 @@ end function;
 */
 
 __random_subspace := function (W, s)
-assert s le Dimension (W);
+assert s lt Dimension (W);
      bas := [];
 	 while #bas le s do
           w := Random (W);
